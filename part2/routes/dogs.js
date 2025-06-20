44c39f924a9bb000f55cf416957fbd87c4bb3134
IsumiW <isumicw@gmail.com>
2025-06-20 12:55:31 +0000

    [automatic commit] 2025-06-20 12:55:31 UTC ;; communications error to 208.67.222.222#53: timed out 129.127.202.10

--- part2/routes/dogs.js
+++ part2/routes/dogs.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const db = require('../models/db');
 
 router.get('/my-dogs', async (req, res) => {
-    if(!req.session.user || req.session.user.role !== 'owner')
+    if(!req.session.user || req.session.user.role !== 'owner'){
+        return res.status(403).json({error: 'Unauthorized'});
+    }
+    try
 })

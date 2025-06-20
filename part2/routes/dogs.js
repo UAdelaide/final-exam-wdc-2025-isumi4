033c589a5d527cd953f9967fda67965567410e93
IsumiW <isumicw@gmail.com>
2025-06-20 12:55:54 +0000

    [automatic commit] 2025-06-20 12:55:54 UTC 129.127.202.10

--- part2/routes/dogs.js
+++ part2/routes/dogs.js
@@ -6,5 +6,8 @@ router.get('/my-dogs', async (req, res) => {
     if(!req.session.user || req.session.user.role !== 'owner'){
         return res.status(403).json({error: 'Unauthorized'});
     }
-    try
+    try{
+        const ownerId = req.session.user.id;
+        const [rows] = await db.query('SELECT dog_id, name ')
+    }
 })

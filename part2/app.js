e8e41f5d954a46bf4cbd20c8fe5a884dbd529e86
IsumiW <isumicw@gmail.com>
2025-06-20 12:57:16 +0000

    [automatic commit] 2025-06-20 12:57:15 UTC 129.127.202.10

--- part2/app.js
+++ part2/app.js
@@ -23,6 +23,7 @@ app.use(express.static(path.join(__dirname, '/public')));
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
+const dogsRouter = require('./')
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);

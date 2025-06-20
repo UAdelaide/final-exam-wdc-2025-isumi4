d84daf79826651eb83fe5dac50d9b6095baf1fdf
IsumiW <isumicw@gmail.com>
2025-06-20 12:02:26 +0000

    [automatic commit] 2025-06-20 12:02:26 UTC 129.127.202.10

--- part2/app.js
+++ part2/app.js
@@ -4,6 +4,18 @@ require('dotenv').config();
 
 const app = express();
 
+const session = require('express-session');
+
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'your_secret_key',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60
+  }
+}));
+
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));

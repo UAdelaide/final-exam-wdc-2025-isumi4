e92893ca6911f3890eb1538e48e04370431a828e
IsumiW <isumicw@gmail.com>
2025-06-20 12:56:55 +0000

    [automatic commit] 2025-06-20 12:56:55 UTC 129.127.202.10

--- part2/routes/dogs.js
+++ part2/routes/dogs.js
@@ -11,6 +11,8 @@ router.get('/my-dogs', async (req, res) => {
         const [rows] = await db.query('SELECT dog_id, name FROM Dogs WHERE owner_id = ?', [ownerId]);
         res.json(rows);
     } catch(error){
-        res.status(500).json({error: 'Failed to fetch dogs'})
+        res.status(500).json({error: 'Failed to fetch dogs'});
     }
-})
+});
+
+module.exports = router;

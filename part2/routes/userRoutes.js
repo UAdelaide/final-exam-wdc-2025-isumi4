aff919d13c328c2044d92194c1414d16dc043f5c
IsumiW <isumicw@gmail.com>
2025-06-20 12:09:17 +0000

    [automatic commit] 2025-06-20 12:09:17 UTC 129.127.202.10

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -70,8 +70,9 @@ router.post('/logout', (req,res) => {
     if(err){
       return res.status(500).json({error: 'Logout failed'});
     }
-    res.clearCookie('connect.sid')
-  })
-})
+    res.clearCookie('connect.sid');
+    res.json({message: 'Logged out successfully'});
+  });
+});
 
 module.exports = router;

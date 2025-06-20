42dcf21e8ff181431f15f77ae111d7a8dcd66e8a
IsumiW <isumicw@gmail.com>
2025-06-20 13:01:07 +0000

    [automatic commit] 2025-06-20 13:01:07 UTC ;; communications error to 208.67.222.222#53: timed out 129.127.202.10

--- part2/routes/dogs.js
+++ part2/routes/dogs.js
@@ -7,13 +7,16 @@ router.get('/my-dogs', async (req, res) => {
         return res.status(403).json({error: 'Unauthorized'});
     }
     const ownerId = req.session.user.id;
-    try{
-        const ownerId = req.session.user.id;
-        const [rows] = await db.query('SELECT dog_id, name FROM Dogs WHERE owner_id = ?', [ownerId]);
-        res.json(rows);
-    } catch(error){
-        res.status(500).json({error: 'Failed to fetch dogs'});
-    }
+    try {
+        const [dogs] = await db.query(
+          'SELECT dog_id, name FROM Dogs WHERE owner_id = ?',
+          [ownerId]
+        );
+        res.json(dogs);
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Failed to fetch dogs' });
+      }
 });
 
 module.exports = router;

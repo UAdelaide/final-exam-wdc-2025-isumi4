0959f3d9b868f2635c1bc8b07d70d48197d84c2a
IsumiW <isumicw@gmail.com>
2025-06-20 11:42:02 +0000

    [automatic commit] 2025-06-20 11:42:02 UTC 129.127.202.10

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -50,7 +50,7 @@ router.post('/login', async (req, res) => {
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
-    console.log()
+    console.log(user.rows[0]);
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     console.error('Login error:', error);

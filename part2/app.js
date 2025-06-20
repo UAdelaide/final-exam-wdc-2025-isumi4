88b1f21874587c97ef864562ca370502c066cfcd
IsumiW <isumicw@gmail.com>
2025-06-20 09:26:19 +0000

    [automatic commit] 2025-06-20 09:26:19 UTC ;; communications error to 208.67.222.222#53: timed out ;; communications error to 208.67.222.222#53: timed out 129.127.202.10

--- part2/app.js
+++ part2/app.js
@@ -1,20 +1,19 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
-
-app.use(logger('dev'));
+// Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Routes
+const walkRoutes = require('./routes/walkRoutes');
+const userRoutes = require('./routes/userRoutes');
 
+app.use('/api/walks', walkRoutes);
+app.use('/api/users', userRoutes);
+
+// Export the app instead of listening here
 module.exports = app;

a6637ae125a143304eda0ab1293abf16d456ab90
IsumiW <isumicw@gmail.com>
2025-06-21 04:43:51 +0000

    [automatic commit] 2025-06-21 04:43:51 UTC ;; communications error to 208.67.222.222#53: timed out 129.127.202.10

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -66,7 +66,7 @@ router.post('/login', async (req, res) => {
 });
 
 function ensureOwner(req, res, next){
-  
+  if(req.session && req.session.user && req.session.user.role === 'owner')
 }
 
 router.post('/logout', (req,res) => {

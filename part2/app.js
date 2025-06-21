88bdfa78aa96d5d186a1a2321e2dbac1243159f3
IsumiW <isumicw@gmail.com>
2025-06-21 04:51:39 +0000

    [automatic commit] 2025-06-21 04:51:39 UTC ;; communications error to 208.67.222.222#53: timed out 129.127.202.10

--- part2/app.js
+++ part2/app.js
@@ -31,26 +31,5 @@ app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/dogs', require('./routes/dogs'));
 
-function ensureOwner(req, res, next){
-    if(req.session && req.session.user && req.session.user.role === 'owner'){
-      next();
-    } else{
-      res.redireect('/');
-    }
-  }
-  function ensureWalker(req, res, next){
-    if(req.session && req.session.user && req.session.user.role === 'walker'){
-      next();
-    } else{
-      res.redireect('/');
-    }
-  }
-
-  app.get('/owner-dashboard.html', ensureOwner, (req, res) => {
-    res.sendFile(__dirname + '/public/owner-dashboard.html');
-  });
-  app.get('/walker-dashboard.html', ensureWalker, (req, res) => {
-    res.sendFile(__dirname + '/public/walker-dashboard.html');
-  });
 // Export the app instead of listening here
 module.exports = app;
